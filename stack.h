e191bb8e5c875bdb3ff771cb214943d6fd512c45
alexandranicolecajes <48707419+alexandranicolecajes@users.noreply.github.com>
2019-05-30 15:48:03 +0800

    Update stack.h

--- stack.h
+++ stack.h
@@ -1,25 +1,9 @@
-/*
- *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #ifndef _STACK_H_
 #define _STACK_H_
 
 #include "ast.h"
 
-#define STACK_SIZE 256 
+#define STACK_SIZE 1000 
 
 typedef struct Stack {
 	BTree *table[STACK_SIZE];
