0b114692c8d4b3b8251c596d66a3f4aa647c7a7c
alexandranicolecajes <48707419+alexandranicolecajes@users.noreply.github.com>
2019-05-30 15:48:58 +0800

    Update stack.c

--- stack.c
+++ stack.c
@@ -1,19 +1,3 @@
-/*
- *  Copyright (c) 2015 Ryan McCullagh <me@ryanmccullagh.com>
- *
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include <stddef.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -35,7 +19,6 @@ BTree* stack_pop(Stack* self)
 	if(self->sp == 0) {
 		return self->table[0];
 	} else if(self->sp > 0) {
-		// TODO test this, valid index ranges, are 0...255. self->sp's max value is 256
 		return self->table[--self->sp];
 	} else {
 		printf("error: stack underflow\n");
